4e3297fa54a3ae517801946503c5c00e0fd954b3
Alejandro Dahlgren <alejandro.dahlgren@gmail.com>
2024-05-17 16:37:34 +0200

    changed env and config, added theme

---
+++ src/theme/material-theme.js
@@ -0,0 +1,21 @@
+import { createTheme } from '@mui/material';
+
+export const themeOptions = createTheme({
+    palette: {
+        primary: {
+            main: '#1bd760',
+            light: '#39d472',
+            dark: '#3b5249',
+            constrastText: '#ffffff',
+        },
+        background: {
+            default: '#ffffff',
+            secondary: '#121212',
+        },
+        text: {
+            primary: '#ffffff',
+            secondary: '#b3b3b3'
+        },
+        divider: '#292929',
+    }
+});
