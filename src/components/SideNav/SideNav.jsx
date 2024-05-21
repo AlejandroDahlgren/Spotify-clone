32fc3e1e239d0a292e92dfd8e54b61086f3e2e70
Alejandro Dahlgren <alejandro.dahlgren@gmail.com>
2024-05-21 14:43:27 +0200

    fixing the playlist and caring for my daughter

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -2,14 +2,19 @@ import { useState, useEffect } from 'react';
 import { Box, Divider } from '@mui/material';
 import NavItem from '../NavItem/NavItem';
 import HomeIcon from '@mui/icons-material/Home';
+import NavPlaylist from '../NavPlaylist/NavPlaylist';
 
 const SideNav = ({ spotifyApi, token }) => {
+	const [playlists, setPlaylists] = useState([]);
+	const [loading, setLoading] = useState(true);
+
 	useEffect(() => {
 		async function getPlaylists() {
 			if (!spotifyApi) return;
 
 			const data = await spotifyApi.getUserPlaylists();
-			console.log(data.body);
+			setPlaylists(data.body.items)
+			setLoading(false)
 		}
 
 		getPlaylists();
@@ -28,11 +33,13 @@ const SideNav = ({ spotifyApi, token }) => {
 			<Box p={3}>
 				<img src="/Spotify_Logo.png" alt="Spotify logo" width={'75%'} />
 			</Box>
-			<NavItem name='Home' Icon={HomeIcon} target="/" />
+			<NavItem name="Home" Icon={HomeIcon} target="/" />
 			<Box p={3} py={1}>
 				<Divider sx={{ backgroundColor: '#ffffff40' }} />
 			</Box>
-			<Box sx={{ overflowY: 'auto', flex: 1 }}>{/**Playlist */}</Box>
+			<Box sx={{ overflowY: 'auto', flex: 1 }}>
+				<NavPlaylist loading={false} name='pop' id='123'/>
+			</Box>
 		</Box>
 	);
 };
