ea04ac68d6ee01a0a0d444b48c3c1d1c8bc670f7
Alejandro Dahlgren <alejandro.dahlgren@gmail.com>
2024-05-22 20:45:25 +0200

    the song just wont play... ffs

--- src/pages/Playlist.jsx
+++ src/pages/Playlist.jsx
@@ -1,44 +1,48 @@
-import { Avatar, Box, Typography } from '@mui/material';
+import { Avatar, Box, Skeleton, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
-import { useParams} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useCallback } from 'react';
+import SongTable from '../components/SongTable/SongTable';
 
 const Playlist = ({ spotifyApi, token }) => {
 	const [playlistinfo, setplaylistinfo] = useState();
 	const [songs, setSongs] = useState([]);
+	const [status, setStatus] = useState({ isLoading: true, isError: null });
 	const { id } = useParams();
-	
 
 	const formatSongs = useCallback(
 		(items) =>
-		items.map((item, i) => {
-		   console.log({ item, i});
-		   const {track} = item; 
-		   track.contextUri = `spotify:playlist:${id}`;
-		   track.position = i;
-		   return track;
-   }), [id])
+			items.map((item, i) => {
+				const { track } = item;
+				track.contextUri = `spotify:playlist:${id}`;
+				track.position = i;
+				return track;
+			}),
+		[id]
+	);
 
 	useEffect(() => {
 		const getData = async () => {
+			setStatus({ isLoading: true, isError: null });
 			try {
 				const playlistDetails = await spotifyApi.getPlaylist(id);
 				setplaylistinfo({
 					image: playlistDetails.body.images[0].url,
 					name: playlistDetails.body.name
-
 				});
 				const { items } = playlistDetails.body.tracks;
 
 				const formattedSongs = formatSongs(items);
 				setSongs(formattedSongs);
-		} catch(e) {
-			console.error(e)
-		}
-	}
-		getData()
-
-	}, [id, formatSongs]);
+			} catch (e) {
+				console.error(e);
+				setStatus({ isLoading: false, isError: e });
+			}
+		};
+		getData().finally(() => {
+			setStatus({ isLoading: false, isError: null });
+		});
+	}, [id, formatSongs, spotifyApi, token]);
 
 	return (
 		<Box id="Playlist__page" sx={{ backgroundColor: 'background.paper', flex: 1, overflowY: 'auto' }}>
@@ -52,22 +56,34 @@ const Playlist = ({ spotifyApi, token }) => {
 					alignItems: { xs: 'flex-start', md: 'flex-end', xl: 'center' },
 					gap: 3,
 					boxSizing: 'border-box',
-					flexDirection: {xs: 'column', md: 'row'}
+					flexDirection: { xs: 'column', md: 'row' }
 				}}
 			>
-				<Avatar
-					src={playlistinfo?.image}
-					variant="square"
-					alt={playlistinfo?.name}
-					sx={{ boxShadow: 15, width: { xs: '100%', md: 235 }, height: { xs: '100%', md: 235 } }}
-				/>
+				{status.isLoading ? (
+					<Skeleton
+						variant="square"
+						sx={{ boxShadow: 15, width: { xs: '100%', md: 235 }, height: { xs: '100%', md: 235 } }}
+					/>
+				) : (
+					<Avatar
+						src={playlistinfo?.image}
+						variant="square"
+						alt={playlistinfo?.name}
+						sx={{ boxShadow: 15, width: { xs: '100%', md: 235 }, height: { xs: '100%', md: 235 } }}
+					/>
+				)}
 				<Box>
 					<Typography sx={{ fontSize: 12, fontWeight: 'bold', color: 'text.primary' }}>Playlist</Typography>
-					<Typography sx={{ fontSize: { xs: 42, md: 72 }, fontWeight: 'bold', color: 'text.primary' }}>
-					{playlistinfo?.name}
-					</Typography>
+					{status.isLoading ? (
+						<Skeleton variant='text' sx={{ fontSize: { xs: 42, md: 72 }, width: 200}} />
+					) : (
+						<Typography sx={{ fontSize: { xs: 42, md: 72 }, fontWeight: 'bold', color: 'text.primary' }}>
+							{playlistinfo?.name}
+						</Typography>
+					)}
 				</Box>
 			</Box>
+			<SongTable songs={songs} loading={status.isLoading} spotifyApi={spotifyApi}  />
 		</Box>
 	);
 };
