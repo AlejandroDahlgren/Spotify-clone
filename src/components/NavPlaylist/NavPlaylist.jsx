8eaf7499e846ea3c9104be7c8eec37f0fbe1b060
Alejandro Dahlgren <alejandro.dahlgren@gmail.com>
2024-05-25 16:37:06 +0200

    dasd

--- src/components/NavPlaylist/NavPlaylist.jsx
+++ src/components/NavPlaylist/NavPlaylist.jsx
@@ -1,10 +1,11 @@
-import { Box, Skeleton } from "@mui/material";
-import { NavLink } from "react-router-dom";
-import './NavPlaylist.css'
+import { Box, Skeleton } from '@mui/material';
+import { NavLink } from 'react-router-dom';
+import './NavPlaylist.css';
 
-const NavPlaylist = ({name, id, loading}) => {
-    return <NavLink className='playlist_navlink' to={loading ? '' : `playlist/${id}`} style={{textDecoration:'none'}}>
-        <Box
+const NavPlaylist = ({ name, id, loading }) => {
+	return (
+		<NavLink className="playlist_navlink" to={loading ? '' : `playlist/${id}`} style={{ textDecoration: 'none' }}>
+			<Box
 				px={3}
 				py={1}
 				sx={{
@@ -16,8 +17,8 @@ const NavPlaylist = ({name, id, loading}) => {
 			>
 				{loading ? <Skeleton variant="text" sx={{ fontSize: 10 }} /> : name}
 			</Box>
-    </NavLink>
-    
-}
+		</NavLink>
+	);
+};
 
 export default NavPlaylist;
